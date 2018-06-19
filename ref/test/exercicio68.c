d13289376dade4167215ca1bdb1abb22dd2b6d95
paulosergioduff <paulosergioduff@gmail.com>
2018-06-19 13:54:29 -0300

    GitFlash

--- ref/test/exercicio68.c
+++ ref/test/exercicio68.c
@@ -1,30 +1,40 @@
-/*Uma loja está levantando o valor total 
-de todas as mercadorias em estoque. 
-Escreva um algoritmo que permita a entrada das seguintes informações: 
-a) o número total de mercadorias no estoque; 
-b) o valor de cada mercadoria. Ao final imprimir o valor 
-total em estoque e a média de valor das mercadorias.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
 
-
-int main(){
-    int numeroTotalMercadoria;
-    double valorMercadoria;
+int numeroTotalMercadoria;
+ 	double valorMercadoria;
     double valorTotalEmEstoque = 0;
     double mediaValorMercadoria =0;
-    
-    int soma=0;
-    
-    
-    printf("informe o numero total de mercadoria no estoque");
-    scanf("%f", &numeroTotalMercadoria);
 
-    printf("informe o valor de cada mercadoria");
-        scanf("%f",&valorMercadoria );
-    
-   
-    
+int soma;
+int i;
+
+int totalEstoque(){
+   //int contador; //variÃ¡vel de controle do loop
+              for(i = 1; i <= numeroTotalMercadoria; i++)
+              {
+                  printf("informe o valor de cada mercadoria");
+                  scanf("%lf",&valorMercadoria );
+              }
+
+              soma = soma +i;
+        valorTotalEmEstoque = numeroTotalMercadoria * valorMercadoria;
+        mediaValorMercadoria = valorTotalEmEstoque / soma;
+
+        printf("O valor total em estoque Ã© de %lf:", valorTotalEmEstoque);
+        
+        printf("A media de valor da mercadoria Ã© de %lf ", mediaValorMercadoria);
+
+        return valorTotalEmEstoque; 
+}
+
+void main(){
+    printf("informe o numero total de mercadoria no estoque\n");
+	scanf("%destoque:\n", &numeroTotalMercadoria);
+
+			
+        totalEstoque();
+
+
 }
---
+++ ref/test/lixo/altEstoque.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char qtd_merc; char qtd_itens; char qtd_acum;
+    int val_uni; int val_tot, int media;
+
+int main(){
+    
+
+    }
