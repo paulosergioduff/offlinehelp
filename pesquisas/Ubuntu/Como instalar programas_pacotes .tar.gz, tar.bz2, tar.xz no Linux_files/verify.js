9df77c1659f02c1772d899d3d76df85c066df93d
paulosergioduff <paulosergioduff@gmail.com>
2018-06-29 12:05:48 -0300

    GitFlash

---
+++ pesquisas/Ubuntu/Como instalar programas_pacotes .tar.gz, tar.bz2, tar.xz no Linux_files/verify.js
@@ -0,0 +1,12 @@
+var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("79ba18d5440844588a5e324c64b9d5b3",false);
+
+
+try{__tagObject_callback_551421478794({ImpressionID:"79ba18d5440844588a5e324c64b9d5b3", ServerPublicDns:"tps612.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "79ba18d5440844588a5e324c64b9d5b3");}catch(e){}
+try{__verify_callback_551421478794({
+ResultID:1,
+Passback:"%3Ca%20href%3D%22https%3A%2F%2Fwww.redcross.org%2Fcharitable-donations%22%20target%3D%22_blank%22%3E%0D%0A%3Cimg%20src%3D%22https%3A%2F%2Fa248.e.akamai.net%2Fappnexus.download.akamai.com%2F89298%2Fadnexus-prod%2Fp%2Fbb%2Fd9%2Fb0%2F85%2Fbbd9b08591b886903e1fc3b7dd8c0f7f.jpg%22%20width%3D%22728%22%20height%3D%2290%22%3E%0D%0A%3C%2Fa%3E",
+AdWidth:728,
+AdHeight:90});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "79ba18d5440844588a5e324c64b9d5b3");}catch(e){}
